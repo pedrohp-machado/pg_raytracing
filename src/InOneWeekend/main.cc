7fe98d1e1de2da064112d5882f62c4a9fe49b1b4
renangrothe <renansuana@gmail.com>
2025-07-23 18:58:33 -0300

    nova cena

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -7,6 +7,8 @@
 //
 // You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
 // along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+// FORK para o projeto de processamento gráfico.
 //==============================================================================================
 
 #include "rtweekend.h"
@@ -21,15 +23,29 @@
 int main() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // alterando a cor do chão dentro de color()
+    auto ground_material = make_shared<lambertian>(color(0.2, 0.3, 0.1));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    // esfera de vidro
+    auto material_glass = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material_glass));
+
+    //esfera fosca
+    auto material_lambertian = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material_lambertian));
+
+    //esfera metálica
+    auto material_metal = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material_metal));
+
+    // menos esferas aleatórias
+    for (int a = -6; a < 6; a++) {
+        for (int b = -6; b < 6; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+	    // esferas aleatórias não devem surgir no lugar das grandes
+            if ((center - point3(4, 0.2, 0)).length() > 0.9 && (center - point3(-4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
 
                 if (choose_mat < 0.8) {
@@ -65,16 +81,16 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 1200;
-    cam.samples_per_pixel = 10;
-    cam.max_depth         = 20;
+    cam.samples_per_pixel = 100; //menos ruído (10 -> 100)
+    cam.max_depth         = 50; //melhor resultado no vidro
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
+    cam.vfov     = 25;
+    cam.lookfrom = point3(0, 2, 13); // mais centralizado
+    cam.lookat   = point3(0, 1, 0); 
     cam.vup      = vec3(0,1,0);
 
-    cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+    cam.defocus_angle = 0.5;
+    cam.focus_dist    = 13.0; // 10->13
 
     cam.render(world);
 }
